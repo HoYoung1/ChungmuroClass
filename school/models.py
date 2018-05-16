616280a08cdb164e8c4512b1de3f873704e2d6ff
KimHoYoung <empire1641@gmail.com>
2018-05-16 14:15:06 +0900

    lecture의 state 추가

---
+++ school/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=64)
+    name = models.CharField(max_length=12)
+    img_url = models.CharField(max_length=64)
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Lecture(models.Model):
+    professor = models.CharField(max_length=12)
+    class_name = models.CharField(max_length=32)
+    students = models.ManyToManyField(Student, null=True, blank=True)
+    class_start = models.DateTimeField(default=timezone.now)
+    regDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.class_name + '/' + self.professor
+
+#현재시각이 5시이면 시작시간이 4시이전의 강의는 다 종료처리한다
+    def state_lecture(self):
+        if self.class_start < datetime.now() - datetime.timedelta(hours=1):
+            return "end"
+        else:
+            return "on"
