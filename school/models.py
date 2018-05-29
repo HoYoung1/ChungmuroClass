06545b1dc948ec04084bc4def13b1099e83ff0e6
HoYoung1 <empire1641@gmail.com>
2018-05-29 14:47:02 +0900

    check 적용,aws rekognition api 적용, 이미지 crop

--- school/models.py
+++ school/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from django.db import models
 from django.utils import timezone
+from school.tasks import say_hello, insert_check
+import os
 
 
 class Student(models.Model):
@@ -13,6 +15,14 @@ class Student(models.Model):
         return self.name
 
 
+    def save(self, *args, **kwargs):
+        print(self.name+" 학생이 등록되었습니다. ")
+        super(Student, self).save(*args, **kwargs)
+        dirname = str(self.id) + "_" + str(self.name)
+        if not os.path.exists("userImg/"+dirname):
+            os.mkdir("userImg/"+dirname)
+
+
 class Lecture(models.Model):
     professor = models.CharField(max_length=12)
     class_name = models.CharField(max_length=32)
@@ -20,10 +30,28 @@ class Lecture(models.Model):
     class_start = models.DateTimeField(default=timezone.now)
     regDate = models.DateTimeField(auto_now_add=True)
 
+    def __init__(self, *args, **kwargs):
+        super(Lecture, self).__init__(*args, **kwargs)
+
     def __str__(self):
         return self.class_name + '/' + self.professor
 
-#현재시각이 5시이면 시작시간이 4시이전의 강의는 다 종료처리한다
+    def save(self, *args, **kwargs):
+        super(Lecture, self).save(*args, **kwargs)
+        print("강의번호 : ", self.id)
+        insert_check(self.id,schedule=self.class_start) #해당시간되면 작동하게??
+
+        #To do
+        #시간이 되면 반복문 돌면서 체크되게 하면될듯
+        #스레드로 백그라운드로 시켜야합니다..1분간격으로 check db에 넣으면 되며
+        #사진을 잘라서 이미지 잘려서 디렉토리에 들어가게 하면됩니다
+        #def 로 백그라운드 하나 더 정의해야할듯
+
+
+
+
+
+#현재시각이 5시이면 시작시간이 4시이전의 강의는 다 종료처리한다. 참가 가능수업과 종료수업 구별하기위해. 참가가능수업은 한개만가능함
     def state_lecture(self):
         if self.class_start < datetime.now() - timedelta(hours=1):
             return "end"
